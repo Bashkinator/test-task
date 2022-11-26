a4eed7339b59442f900c17ef08f1d983d3d62f1e
Bashkinator <bashkinator@gmail.com>
2022-11-26 21:39:31 +0300

    add strings generation (could be faster)

--- main.py
+++ main.py
@@ -1,4 +1,36 @@
 import argparse
+import uuid
+
+
+class RandomGenerator:
+
+    def __init__(self):
+        self.unique_strings_generated = set()
+
+    @staticmethod
+    def get_random_string():
+        # Returns the random string, based on generated UUID4.
+        return uuid.uuid4().hex.encode()
+
+    def get_unique_random_string(self):
+        # Returns the random string, which is unique for that RandomGenerator instance.
+        # Saves generated string in self.unique_strings_generated
+        while True:
+            rand_str = self.get_random_string()
+            if rand_str not in self.unique_strings_generated:
+                self.unique_strings_generated.add(rand_str)
+                break
+
+    def get_unique_random_strings(self, count):
+        # Returns a set of random strings, that are unique for that RandomGenerator instance.
+        # Saves generated strings in self.unique_strings_generated
+        old_strings = set(self.unique_strings_generated)  # Copy previous strings
+        new_length = len(old_strings) + count
+        # Generating strings until the target length is reached
+        while len(self.unique_strings_generated) < new_length:
+            rand_str = self.get_random_string()
+            self.unique_strings_generated.add(rand_str)
+        return self.unique_strings_generated - old_strings  # Returns only new strings
 
 
 def init_argparse() -> argparse.ArgumentParser:
@@ -25,7 +57,7 @@ def main() -> None:
     if args.parse:
         pass  # do parsing
     else:  # creating is default option
-        pass # do creating
+        pass  # do creating
 
 
 if __name__ == "__main__":
