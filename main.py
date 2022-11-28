2fde1486989786f2d3a94ebd84b27f3bf1fe983b
Bashkinator <bashkinator@gmail.com>
2022-11-28 16:56:39 +0300

    Creating zip files with xmls

--- main.py
+++ main.py
@@ -1,18 +1,31 @@
 import argparse
+import io
+import itertools
 import logging
 import os.path
 import pathlib
 import random
 import sys
+import threading
 import uuid
+import zipfile
+from concurrent.futures import ProcessPoolExecutor, as_completed, ThreadPoolExecutor
 
 from lxml import etree
 
-logger = logging.getLogger('test-task')
-logger.setLevel(logging.INFO)
-handler = logging.StreamHandler(stream=sys.stdout)
-handler.setFormatter(logging.Formatter(fmt='[%(asctime)s: %(levelname)s] %(message)s'))
-logger.addHandler(handler)
+
+def init_logger(level=logging.INFO):
+    logger = logging.getLogger('test-task')
+    logger.setLevel(level)
+    handler = logging.StreamHandler(stream=sys.stdout)
+    handler.setFormatter(logging.Formatter(fmt='[%(asctime)s: %(levelname)s] %(message)s'))
+    logger.addHandler(handler)
+    return logger
+
+
+# not thread/process safe
+def get_logger():
+    return logging.getLogger('test-task')
 
 
 class TestObject:
@@ -51,13 +64,12 @@ class TestObject:
 
 
 class RandomGenerator:
-
     unique_strings_generated = set()
 
     @staticmethod
     def get_random_string():
         # Returns the random string, based on generated UUID4.
-        return uuid.uuid4().hex.encode()
+        return uuid.uuid4().hex
 
     @classmethod
     def get_unique_random_string(cls):
@@ -82,8 +94,39 @@ class RandomGenerator:
         return cls.unique_strings_generated - old_strings  # Returns only new strings
 
 
-def create_files(zip_count, xml_count, out_dir):
-    pass
+def create_testfile(object_id, file, lock):
+    obj_tree = TestObject(object_id).serialize()
+    obj_str = etree.tostring(obj_tree, xml_declaration=True)
+    with lock:
+        file.writestr(f"{object_id}.xml", obj_str)
+    return object_id
+
+
+def create_zipfile(name, object_ids, out_dir):
+    zip_buffer = io.BytesIO()
+    lock = threading.Lock()
+    with zipfile.ZipFile(zip_buffer, mode="w", compression=zipfile.ZIP_DEFLATED) as zf:
+        with ThreadPoolExecutor(10) as exe:
+            futures = [exe.submit(create_testfile, object_id, zf, lock) for object_id in object_ids]
+            for future in as_completed(futures):
+                _ = future.result()
+    filename = os.path.join(out_dir, name)
+    with open(filename, "wb") as f:
+        f.write(zip_buffer.getvalue())
+    return [name, out_dir, object_ids]
+
+
+def create_files(zip_count, xml_count, out_dir, worker_count):
+    all_object_ids = RandomGenerator.get_unique_random_strings(zip_count * xml_count)
+    object_ids_iter = iter(all_object_ids)
+    with ProcessPoolExecutor(worker_count) as exe:
+        futures = list()
+        for i in range(0, zip_count):
+            objects_ids = list(itertools.islice(object_ids_iter, xml_count))  # get chunk of names from set iterator
+            futures.append(exe.submit(create_zipfile, f"{i+1}.zip", objects_ids, out_dir))
+        for future in as_completed(futures):
+            res = future.result()
+            get_logger().debug(res)
 
 
 def parse_files(src_dir, out_dir):
@@ -105,6 +148,8 @@ def init_argparse() -> argparse.ArgumentParser:
     parser.add_argument('-s', '--source-dir', action='store', default='./out')
     parser.add_argument('-o', '--output-dir', action='store', default='./out')
 
+    parser.add_argument("-w", "--worker-count", action='store', type=int, default=4)
+
     parser.add_argument('-v', '--verbose', action='store_true')
 
     return parser
@@ -114,8 +159,7 @@ def main() -> None:
     parser = init_argparse()
     args = parser.parse_args()
 
-    if args.verbose:
-        logger.setLevel(logging.DEBUG)
+    logger = init_logger(logging.DEBUG if args.verbose else logging.INFO)
 
     if args.parse:
         src_dir = os.path.abspath(args.source_dir)
@@ -129,7 +173,7 @@ def main() -> None:
         out_dir = os.path.abspath(args.output_dir)
         pathlib.Path(out_dir).mkdir(parents=True, exist_ok=True)
         logger.info(f"Creating {args.zip_count} zip-files with {args.xml_count} xml-files in each")
-        create_files(args.zip_count, args.xml_count, out_dir)  # do creating
+        create_files(args.zip_count, args.xml_count, out_dir, args.worker_count)  # do creating
         logger.info(f"{args.zip_count} zip-files with {args.xml_count} xml-files in each are created in {out_dir}")
 
 
